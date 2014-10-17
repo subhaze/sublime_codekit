4f1d00f2f5eb4657dc6d11837e48741e23ad79c0
subhaze <jmrussell22@gmail.com>
2014-10-17 12:17:26 -0400

    Settings file and more
    - Adds a settings file to allow disabling of the auto features
    - Updates README.md
    - Adds a menu item to the main menu

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 #[CodeKit](https://incident57.com/codekit/) 2 plug-in for Sublime Text
 
-###Current CodeKit features
+##Current CodeKit features
 - Automatically switch CodeKit project based on current view
-- - Todo: Add a settings flag to disable this
+- - You can disable this in the settings
 - Pause/Unpause CodeKit based on Sublime Text focus
-- - __Todos for pausing__
-- - Add Pause/Unpause to Command Palette
-- - Add setting to disable auto pause/unpause based on Sublime Text focus
+- - __Todo:__ Add Pause/Unpause to Command Palette
+- - __Todo:__ Add setting to disable auto pause/unpause based on Sublime Text focus
 - Add folder from Sublime Text's sidebar as a CodeKit project
 - Add folder from Sublime Text's sidebar as a CodeKit framework
 - Select a project in CodeKit base on the active view
@@ -16,6 +15,34 @@
 - Refresh browser
 - Reload style sheets
 
-###Usage
+----
 
-Just type 'Codekit' in the Command Palette to see the list of commands available.
+##Installation
+
+You'll need [Package Control](https://sublime.wbond.net/installation) installed. Once you've got that setup all you have to do is open the Command Palette (super+shift+p) then type `install package` hit enter, then search for 'CodeKit Commands' and select it.
+
+----
+
+##How it works
+
+###Auto-active CodeKit projects
+When you open a project in Sublime Text, CodeKit will automatically open for you (if it's not already open) and then set the active project in CodeKit based on the project you're working on. If you have a few different projects open, this plug-in will automatically change what project is active in CodeKit based on what file you're currently editing.
+
+###Manually activate CodeKit projects
+You can disable auto-project switching by disabling `Preferences>Package Settings>CodeKit Commands>Enable Auto Switch CodeKit Project` and you can still change the active project in CodeKit from Sublime via the Command Palette commands `CodeKit Select project` or `CodeKit Select framework`.
+
+###Adding projects to CodeKit
+If you're currently working on a project or framework that CodeKit doesn't know about, simply fire up the Command Palette and run the command `CodeKit Add project` or `CodeKit Add framework`. You'll be presented with a list of folders specific to your current Sublime project to select from and add to CodeKit.
+
+###Auto-pausing CodeKit
+By default this plug-in will pause CodeKit when you're not focused on Sublime Text. This can be useful if you use the CLI or third-party tools to handle source control.
+
+CodeKit states:
+>Before you perform any action that will change large numbers of files at once (switching branches, rebasing, pull requests, etc.) you MUST tell CodeKit to ignore file changes.
+
+If you'd rather handle toggling the paused state of CodeKit manually you can disable this feature via `Preferences>Package Settings>CodeKit Commands>Enable Auto Pausing` and run the `CodeKit pause` and `CodeKit unpause` commands from the Command Palette. __Heads Up__ These two commands do nothing if you have auto-pausing enabled (which it is by default).
+
+More details about why it can be good to pause and other critical things to know about CodeKit can be found [here](http://incident57.com/codekit/help.html#critical-things).
+
+###Handling browsers
+You also have access to browser refreshing, style sheet reloading, browser specific previewing all from the Command Palette. Just type `CodeKit` to see the full list. Any new commands added to Command Palette will always be prefixed with `CodeKit` for easy viewing/access.
--- codekit.py
+++ codekit.py
@@ -1,32 +1,86 @@
 import sublime, sublime_plugin, os
 
+#
+# Helper Utils
+#
+class _Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(_Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
 
-class CodekitEventListener(sublime_plugin.EventListener):
+
+class Singleton(_Singleton('SingletonMeta', (object,), {})):
+    pass
+#
+# Ends Helper Utils
+#
+
+
+class CodeKitState(Singleton):
+    isST2 = int(sublime.version()) < 3000
+    # Holds the property/values of the Sublime Text Settings file
+    settings = None
+    # Used to know if CodeKit should be paused/unpaused
     is_active = False
     # Used to ensure we're not making requests to CodeKit
     # on every view change in the same project
     folders_key = ''
 
-    def on_activated(self, view):
+    # Handle CodeKit pause/unpause
+    def handle_pausing(self, active=False):
+        if not self.settings.get('pause_codekit_on_view_deactivate', True):
+            return
+        if active: self.unpause_code_kit()
+        else: self.pause_code_kit()
+
+    def unpause_code_kit(self):
         # If we don't see Sublime Text as active, make it so
         # and unpause CodeKit
-        if not self.is_active:
+        if not CodeKitState().is_active:
             os.system("""osascript -e 'tell application "CodeKit" to unpause file watching'""")
-            self.is_active = True
+            CodeKitState().is_active = True
+
+    def pause_code_kit(self):
+        print('pause codekit')
+        if not CodeKitState().is_active:
+            os.system("""osascript -e 'tell application "CodeKit" to pause file watching'""")
+            CodeKitState().is_active = False
+
+    # Handle auto activating CodeKit projects
+    def active_code_kit_project(self):
+        if not self.settings.get('auto_switch_codekit_projects', True):
+            return
+
         current_folders_key = '-'.join(sublime.active_window().folders())
         if current_folders_key != self.folders_key:
             self.folders_key = current_folders_key
             sublime.active_window().run_command('codekit_select_project_from_view')
 
+
+if CodeKitState().isST2:
+    CodeKitState().settings = sublime.load_settings('CodeKit.sublime-settings')
+
+
+def plugin_loaded():
+    CodeKitState().settings = sublime.load_settings('CodeKit.sublime-settings')
+
+
+class CodekitEventListener(sublime_plugin.EventListener):
+
+    def on_activated(self, view):
+        CodeKitState().handle_pausing(active=True)
+        CodeKitState().active_code_kit_project()
+
     def on_deactivated(self, view):
-        self.is_active = False
-        sublime.set_timeout(self.pause_code_kit, 10)
-
-    def pause_code_kit(self):
-        if not self.is_active:
-            os.system("""osascript -e 'tell application "CodeKit" to pause file watching'""")
+        CodeKitState().is_active = False
+        sublime.set_timeout(CodeKitState().handle_pausing, 10)
 
 
+#
+# Add Project/Frame work commands
+# 
 class CodekitAddProjectCommand(sublime_plugin.ApplicationCommand):
     folders = []
 
@@ -57,6 +111,9 @@ class CodekitAddFrameworkCommand(sublime_plugin.ApplicationCommand):
             os.system("""osascript -e 'tell application "CodeKit" to add framework at path "%s"'""" % self.folders[indx])
 
 
+#
+# Select Project/Framework commands
+# 
 class CodekitSelectProjectFromViewCommand(sublime_plugin.ApplicationCommand):
 
     def run(self):
@@ -71,6 +128,25 @@ class CodekitSelectFrameworkFromViewCommand(sublime_plugin.ApplicationCommand):
         os.system("""osascript -e 'tell application "CodeKit" to select framework containing path "%s"'""" % file_name)
 
 
+#
+# Pause/Unpause commands
+# These commands only work IF auto-pausing is disabled
+# 
+class CodekitPauseCommand(sublime_plugin.ApplicationCommand):
+
+    def run(self):
+        CodeKitState().pause_code_kit()
+
+
+class CodekitUnpauseCommand(sublime_plugin.ApplicationCommand):
+
+    def run(self):
+        CodeKitState().unpause_code_kit()
+
+
+#
+# Browser Commands
+# 
 class CodekitPreviewInBrowserCommand(sublime_plugin.ApplicationCommand):
 
     def run(self):
@@ -116,3 +192,20 @@ class CodekitReloadStyleSheetsCommand(sublime_plugin.ApplicationCommand):
 
     def run(self):
         os.system("""osascript -e 'tell application "CodeKit" to refresh browsers by reloading just stylesheets'""")
+
+
+#
+# Settings Helpers
+#
+class CodekitToggleSettingsCommand(sublime_plugin.ApplicationCommand):
+
+    """Enables/Disables settings"""
+
+    def run(self, setting):
+        s = sublime.load_settings('CodeKit.sublime-settings')
+        s.set(setting, not s.get(setting, False))
+        sublime.save_settings('CodeKit.sublime-settings')
+
+    def is_checked(self, setting):
+        s = sublime.load_settings('CodeKit.sublime-settings')
+        return s.get(setting, False)
