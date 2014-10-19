541ab21bb1205492dfbbc3acc08db8bcc34f8e86
subhaze <jmrussell22@gmail.com>
2014-10-19 16:23:54 -0400

    Clean up code a bit, make variable names a bit more meaningful
    Reworked how auto-switching project logic works, now looks for an actual 'config.codekit' file.
    
    The rework also allows views from different projects to be mixed around in the same window
    but still trigger CodeKit to active the correct project.

--- codekit.py
+++ codekit.py
@@ -1,10 +1,13 @@
-import sublime, sublime_plugin, os
+import sublime, sublime_plugin, os, time
 
 #
 # Helper Utils
 #
+
+
 class _Singleton(type):
     _instances = {}
+
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(_Singleton, cls).__call__(*args, **kwargs)
@@ -21,60 +24,95 @@ class CodeKitState(Singleton):
     isST2 = int(sublime.version()) < 3000
     # Holds the property/values of the Sublime Text Settings file
     settings = None
-    # Used to know if CodeKit should be paused/unpaused
-    is_active = False
+    is_paused = False
+    st_view_active = False
     # Used to ensure we're not making requests to CodeKit
     # on every view change in the same project
     folders_key = ''
+    current_path = ''
 
     # Handle CodeKit pause/unpause
-    def handle_pausing(self, active=False):
+    def handle_auto_pausing(self):
         if not self.settings.get('pause_codekit_on_view_deactivate', True):
             return
-        if active: self.unpause_code_kit()
-        else: self.pause_code_kit()
+        if CodeKitState().st_view_active:
+            self.unpause_code_kit()
+        else:
+            self.pause_code_kit()
 
     def unpause_code_kit(self):
         # If we don't see Sublime Text as active, make it so
         # and unpause CodeKit
-        if not CodeKitState().is_active:
+        if CodeKitState().is_paused:
             os.system("""osascript -e 'tell application "CodeKit" to unpause file watching'""")
-            CodeKitState().is_active = True
+            CodeKitState().is_paused = False
 
     def pause_code_kit(self):
-        print('pause codekit')
-        if not CodeKitState().is_active:
+        if not CodeKitState().is_paused:
             os.system("""osascript -e 'tell application "CodeKit" to pause file watching'""")
-            CodeKitState().is_active = False
+            CodeKitState().is_paused = True
 
     # Handle auto activating CodeKit projects
-    def active_code_kit_project(self):
+    def activate_code_kit_project(self, view):
         if not self.settings.get('auto_switch_codekit_projects', True):
             return
+        # We're more than likely working with a view that's not been
+        # saved yet, so bail since there's nothing we can do here.
+        if not view.file_name():
+            return
 
-        current_folders_key = '-'.join(sublime.active_window().folders())
-        if current_folders_key != self.folders_key:
-            self.folders_key = current_folders_key
+        abs_path = view.file_name()
+        path, file_name = os.path.split(abs_path)
+        # Check to see if the current file is within the same
+        # path as a previous check, if so, assume project is active.
+        # This helps reduce the amount of looping around dirs
+        # looking for a config.codekit file
+        if self.current_path and path.startswith(self.current_path):
+            return
+        # Reset the path to nothing since we're now looking for a new
+        # codekit project file
+        self.current_path = ''
+        while path and not self.current_path:
+            if 'config.codekit' in os.listdir(path):
+                self.current_path = path
+            path = '/'.join(path.split('/')[0:-1])
+
+        if self.current_path:
             sublime.active_window().run_command('codekit_select_project_from_view')
 
 
+#
+# Setup Settings file
+#
 if CodeKitState().isST2:
     CodeKitState().settings = sublime.load_settings('CodeKit.sublime-settings')
 
 
 def plugin_loaded():
     CodeKitState().settings = sublime.load_settings('CodeKit.sublime-settings')
+#
+# End Setup Settings File
+#
 
 
 class CodekitEventListener(sublime_plugin.EventListener):
 
     def on_activated(self, view):
-        CodeKitState().handle_pausing(active=True)
-        CodeKitState().active_code_kit_project()
+        CodeKitState().st_view_active = True
+        CodeKitState().handle_auto_pausing()
+        CodeKitState().activate_code_kit_project(view)
 
     def on_deactivated(self, view):
-        CodeKitState().is_active = False
-        sublime.set_timeout(CodeKitState().handle_pausing, 10)
+        CodeKitState().st_view_active = False
+
+        # A bit of a hack due to ST triggering a deactivate then activate on the
+        # same file when you open things like the command palette.
+        # This should reduce a lot of false signaling to the CodeKit API
+        def delayed_code_kit_pause():
+            if not CodeKitState().st_view_active:
+                CodeKitState().handle_auto_pausing()
+
+        sublime.set_timeout(delayed_code_kit_pause, 20)
 
 
 #
@@ -136,11 +174,17 @@ class CodekitPauseCommand(sublime_plugin.ApplicationCommand):
     def run(self):
         CodeKitState().pause_code_kit()
 
+    def is_visible(self):
+        return not CodeKitState().settings.get('pause_codekit_on_view_deactivate')
+
 
 class CodekitUnpauseCommand(sublime_plugin.ApplicationCommand):
 
     def run(self):
         CodeKitState().unpause_code_kit()
+
+    def is_visible(self):
+        return not CodeKitState().settings.get('pause_codekit_on_view_deactivate')
 
 
 #
