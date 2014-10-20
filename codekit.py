31f3fe7fd5501dff3a9c8d1434e36fa74f4d9e49
subhaze <jmrussell22@gmail.com>
2014-10-19 21:39:33 -0400

    Use async actions when possible, Fixes #2

--- codekit.py
+++ codekit.py
@@ -32,16 +32,14 @@ class CodeKit(Singleton):
     current_path = ''
 
     def run_apple_script(self, command):
-        os.system("""osascript -e 'tell application "CodeKit" to %s'""" % command)
-
-    # Handle CodeKit pause/unpause
-    def handle_auto_pausing(self):
-        if not self.settings.get('pause_codekit_on_view_deactivate', True):
-            return
-        if CodeKit().st_view_active:
-            sublime.active_window().run_command('codekit_unpause')
+        tell_code_kit = """osascript -e 'tell application "CodeKit" to %s'""" % command
+        if self.isST2:
+            os.system(tell_code_kit)
         else:
-            sublime.active_window().run_command('codekit_pause')
+            sublime.set_timeout_async(
+                lambda: os.system(tell_code_kit)
+                , 0
+            )
 
     # Handle auto activating CodeKit projects
     def activate_code_kit_project(self, view):
@@ -71,6 +69,32 @@ class CodeKit(Singleton):
         if self.current_path:
             sublime.active_window().run_command('codekit_select_project_from_view')
 
+    # Handle CodeKit pause/unpause
+    def handle_auto_pausing(self):
+        if not self.settings.get('pause_codekit_on_view_deactivate', True):
+            return
+        if CodeKit().st_view_active:
+            sublime.active_window().run_command('codekit_unpause')
+        else:
+            sublime.active_window().run_command('codekit_pause')
+
+    def handle_view_activated(self, view):
+        self.st_view_active = True
+        self.handle_auto_pausing()
+        self.activate_code_kit_project(view)
+
+    def handle_view_deactivated(self, view):
+        self.st_view_active = False
+
+        # A bit of a hack due to ST triggering a deactivate then activate on the
+        # same file when you open things like the command palette.
+        # This should reduce a lot of false signaling to the CodeKit API
+        def delayed_code_kit_pause():
+            if not self.st_view_active:
+                self.handle_auto_pausing()
+
+        sublime.set_timeout(delayed_code_kit_pause, 144)
+
 
 #
 # Setup Settings file
@@ -88,22 +112,18 @@ if CodeKit().isST2:
 class CodekitEventListener(sublime_plugin.EventListener):
 
     def on_activated(self, view):
-        CodeKit().st_view_active = True
-        CodeKit().handle_auto_pausing()
-        CodeKit().activate_code_kit_project(view)
+        if CodeKit().isST2:
+            CodeKit().handle_view_activated(view)
+
+    def on_activated_async(self, view):
+        CodeKit().handle_view_activated(view)
 
     def on_deactivated(self, view):
-        CodeKit().st_view_active = False
+        if CodeKit().isST2:
+            CodeKit().handle_view_deactivated(view)
 
-        # A bit of a hack due to ST triggering a deactivate then activate on the
-        # same file when you open things like the command palette.
-        # This should reduce a lot of false signaling to the CodeKit API
-        def delayed_code_kit_pause():
-            if not CodeKit().st_view_active:
-                CodeKit().handle_auto_pausing()
-
-        sublime.set_timeout(delayed_code_kit_pause, 20)
-
+    def on_deactivated_async(self, view):
+        CodeKit().handle_view_deactivated(view)
 
 #
 # Add Project/Frame work commands
